1d154e4347d77b9e7140ad73af404cbbdbdec905
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-09-23 17:06:25 +0600

    fix: Remove unnecessary comment from USER_ENDPOINTS in users.ts

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -1,5 +1,5 @@
 import { BASE_API_URL } from './base';
 
 export const USER_ENDPOINTS = {
-  GET_PROFILE: `${BASE_API_URL}`, // Dynamic: /${userType}s/get-profile
+  GET_PROFILE: `${BASE_API_URL}`, 
 } as const;
